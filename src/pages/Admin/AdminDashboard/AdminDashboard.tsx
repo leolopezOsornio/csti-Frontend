ba373b4c02793b69d1c0c171ce29cf6a97896ffb
leolopezOsornio <leol83377@gmail.com>
2026-04-18 23:27:02 -0600

    feat(Login): Validacion de usuario Admin/Cliente.

---
+++ src/pages/Admin/AdminDashboard/AdminDashboard.tsx
@@ -0,0 +1,11 @@
+// src/pages/admin/AdminDashboard/AdminDashboard.tsx
+const AdminDashboard = () => {
+    return (
+        <div style={{ padding: '2rem' }}>
+            <h1>Panel de administrador</h1>
+            <p>Bienvenido, eres admin 😎</p>
+        </div>
+    );
+};
+
+export default AdminDashboard;
