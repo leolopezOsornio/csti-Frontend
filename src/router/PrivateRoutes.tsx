174cef54a3a9800db0ea48bcafd9f657e3d21c17
leolopezOsornio <leol83377@gmail.com>
2026-04-18 23:45:09 -0600

    feat(router): Proteccion de rutas de administrador.

--- src/router/PrivateRoutes.tsx
+++ src/router/PrivateRoutes.tsx
@@ -4,8 +4,11 @@ import { useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 
 export const PrivateRoutes = () => {
-    // Leemos el estado real desde nuestro contexto global
-    const { isAuthenticated } = useContext(AuthContext);
+    const { isAuthenticated, isLoading } = useContext(AuthContext);
+
+    if (isLoading) {
+        return <div>Cargando...</div>;
+    }
 
     return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
 };
